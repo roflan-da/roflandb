a8d69ec191b4d0ae93eb5e8e1cfe7dd146939bd1
Mup0c <mir-os@mail.ru>
2018-10-30 22:31:10 +1000

    Some tests added. Bug captured

--- tests/table_test.cpp
+++ tests/table_test.cpp
@@ -5,19 +5,59 @@
 TEST_CASE("IntegerColumn") {
 
     SECTION("trivial") {
-        TableBuilder tableBuilder("Kektable");
-
         IntegerColumn intcol("Test");
-        CHECK(intcol.getName() == "Test");
-        CHECK(intcol.getTypeName() == "INT");
+        REQUIRE(intcol.getName() == "Test");
+        REQUIRE(intcol.getTypeName() == "INT");
     }
 
-}
-
-TEST_CASE("Table") {
 
 }
 
 TEST_CASE("TableBuilder") {
+
+    SECTION("getTableName") {
+        TableBuilder tb("mems");
+        REQUIRE(tb.getTableName() == "mems");
+    }
+
+    SECTION("getColumns_0") {
+        TableBuilder tb("mems");
+        REQUIRE(tb.getColumns().size() == 0);
+    }
+
+    SECTION("getColumns_INT_1") {
+        TableBuilder tb("mems");
+        tb.addColumn("INT", "Test");
+        REQUIRE(tb.getColumns().size() == 1);
+        //REQUIRE(tb.getColumns()[0]->getTypeName() == "INT");
+        //REQUIRE(tb.getColumns()[0]->getName() == "Test");
+    }
+
+    SECTION("getColumns_WRONG_1") {
+        TableBuilder tb("mems");
+        tb.addColumn("abracadabra", "Test");
+        REQUIRE(tb.getColumns().size() == 0);
+    }
+
+    SECTION("getColumns_INT_MAX_INT") {
+        TableBuilder tb("mems");
+        for (int i = 0; i < INT_MAX/10000; ++i) {
+            tb.addColumn("INT", "Test" + std::to_string(i));
+        }
+        std::vector<std::shared_ptr<Column>> columns(tb.getColumns());
+        REQUIRE(tb.getColumns().size() == INT_MAX/10000);
+        for (int i = 0; i < INT_MAX/10000; ++i) {
+            REQUIRE(columns[i]->getTypeName() == "INT");
+            REQUIRE(columns[i]->getName() == "Test" + std::to_string(i));
+
+        }
+
+    }
+
+
+
+}
+
+TEST_CASE("Table") {
 
 }
