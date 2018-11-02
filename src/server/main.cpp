3e78887f3af3f323c18671ff15d28ecba14b7bdf
Mup0c <mir-os@mail.ru>
2018-11-02 13:05:03 +1000

    basic console reading. Waiting for other modules to interact

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,18 +1,18 @@
 #include "main.h"
-#include "storage_engine.h"
-#include <fstream>
-#include <storage_engine.h>
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
     std::cout << "My name is RoflanDB." << std::endl;
     std::cout << "RoflanDB strong!" << std::endl;
 
-    std::string command;
     while (true) {
-        std::cin >> command;
-        std::cout << command;
+        std::string command;
+        std::cout << "Enter query without \\n: \n";
+
+        std::getline(std::cin, command);
+        std::cout << command << " SQL \n";
     }
+
 
 
     return 0;
