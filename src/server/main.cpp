e8a12b93f12527c6047d33ea284b3e23a476d362
Sityaev Artem <luckthemall@gmail.com>
2018-11-02 12:30:23 +1000

    main() fixed

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -7,11 +7,6 @@ int main() {
     std::cout << "Hello, world!" << std::endl;
     std::cout << "My name is RoflanDB." << std::endl;
     std::cout << "RoflanDB strong!" << std::endl;
-    /*//чтение
-    std::ifstream in("My_db.txt");
-    //std::string tables_string;
-    st_e::StorageEngine storageEngine;
-    //in >> tables_string;
 
     std::string command;
     while (true) {
