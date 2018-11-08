660d9ce5beb330a97dad67be45cae8cdf1382e45
Mup0c <mir-os@mail.ru>
2018-11-08 19:40:43 +1000

    removed mock test

--- src/query_conditions/inc/query_conditions.h
+++ src/query_conditions/inc/query_conditions.h
@@ -1,22 +1,33 @@
 #pragma once
 
 #include "condition.h"
-#include <memory>
 
 namespace cond {
-enum ComplexConditionType{
-    AND,
-    OR,
-    NOT
-};
+
+    enum ComplexConditionType{
+        AND,
+        OR,
+        NOT
+    };
 
 
 
     class QueryConditions {
-public:
+    public:
 
-private:
-    std::string table_name_;
-};
+        QueryConditions() = default;
+        //QueryConditions()
+
+        void type(ComplexConditionType type) { type_ = type; }
+        ComplexConditionType type() const { return type_; }
+
+        void first(std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> first) { first_ = first; }
+
+    private:
+
+        ComplexConditionType type_;
+        std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> first_, second_;
+
+    };
 
 }//namespace cond
