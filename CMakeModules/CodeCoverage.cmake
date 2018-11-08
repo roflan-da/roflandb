adb2ca2f928a801a1e8d737c8496b616548b7cf1
Alexey Mogilyovkin <zeroicq@gmail.com>
2018-11-08 23:30:32 +0000

    Coverage

---
+++ CMakeModules/CodeCoverage.cmake
@@ -0,0 +1,16 @@
+find_program(LCOV_PATH lcov)
+find_program(GENHTML_PATH genhtml)
+
+IF(NOT LCOV_PATH)
+    MESSAGE(WARNING "lcov not found!")
+ENDIF() # NOT LCOV_PATH
+
+IF(NOT GENHTML_PATH)
+    MESSAGE(WARNING "genhtml not found!")
+ENDIF() # NOT GENHTML_PATH
+
+add_custom_target(coverage
+        COMMAND "${CMAKE_BINARY_DIR}/bin/tests"
+        COMMAND ${LCOV_PATH} --directory ${CMAKE_BINARY_DIR} --capture --output-file ${CMAKE_BINARY_DIR}/coverage.info
+        COMMAND ${GENHTML_PATH} -o ${CMAKE_BINARY_DIR}/coverage/ ${CMAKE_BINARY_DIR}/coverage.info
+        )
