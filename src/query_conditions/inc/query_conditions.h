20af8e01273bb12e307b712e1f33a4bc60197134
Mup0c <mir-os@mail.ru>
2018-11-08 19:40:43 +1000

    removed mock test

--- src/query_conditions/inc/query_conditions.h
+++ src/query_conditions/inc/query_conditions.h
@@ -1,34 +1,33 @@
 #pragma once
 
 #include "condition.h"
-#include <memory>
 
 namespace cond {
 
-enum ComplexConditionType{
-    AND,
-    OR,
-    NOT
-};
+    enum ComplexConditionType{
+        AND,
+        OR,
+        NOT
+    };
 
 
 
-class QueryConditions {
-public:
+    class QueryConditions {
+    public:
 
-    QueryConditions() = default;
-    //QueryConditions()
+        QueryConditions() = default;
+        //QueryConditions()
 
-    void type(ComplexConditionType type) { type_ = type; }
-    ComplexConditionType type() const { return type_; }
+        void type(ComplexConditionType type) { type_ = type; }
+        ComplexConditionType type() const { return type_; }
 
-    //void first()
+        void first(std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> first) { first_ = first; }
 
-private:
+    private:
 
-    ComplexConditionType type_;
-    std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> first_, second_;
+        ComplexConditionType type_;
+        std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> first_, second_;
 
-};
+    };
 
 }//namespace cond
--- tests/inc/storage_engine_mock.h
+++ tests/inc/storage_engine_mock.h
@@ -3,11 +3,6 @@
 
 class StorageEngineMock : public st_e::IEngineStorage {
 public:
-    void addTable(std::shared_ptr<st_e::Table> table) override;
-    void deleteTable(std::string table_name) override;
-    std::shared_ptr<st_e::Table> getTableByName(std::string table_name) override;
-    void Save() override;
-    void Load() override;
 
 };
 
