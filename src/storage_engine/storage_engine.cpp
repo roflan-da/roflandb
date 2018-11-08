9ed037388f5feafe7c38bada5a91a08b38776416
SergeiZinkovich <my_agent-1@mail.ru>
2018-11-08 19:07:40 +1000

    Column name and type now in pair

--- src/storage_engine/storage_engine.cpp
+++ src/storage_engine/storage_engine.cpp
@@ -38,17 +38,14 @@ namespace st_e {
             TableBuilder tableBuilder(table_name);
             int columns_count;
             in >> columns_count;
-            std::vector<ColumnType> columns_types;
-            std::vector<std::string> columns_names;
+            std::vector<std::pair<ColumnType, std::string>> columns;
             for (int j = 0; j < columns_count; j++){
                 std::string column_name;
                 int type;
                 in >> column_name >> type;
-                columns_types.push_back(static_cast<ColumnType>(type));
-                columns_names.push_back(column_name);
+                columns.push_back({static_cast<ColumnType>(type), column_name});
             }
-            tableBuilder.set_columns_names(columns_names);
-            tableBuilder.set_columns_types(columns_types);
+            tableBuilder.set_columns(columns);
             Table table = tableBuilder.build();
             add_table(std::make_shared<Table>(table));
         }
