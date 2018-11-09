2446a150cacda45d5f803475b88f4775690e67f5
ZeroICQ <zeroicq@gmail.com>
2018-11-09 19:04:04 +1000

    Refactor

--- CMakeModules/CodeCoverage.cmake
+++ CMakeModules/CodeCoverage.cmake
@@ -1,16 +1,20 @@
 find_program(LCOV_PATH lcov)
 find_program(GENHTML_PATH genhtml)
 
-IF(NOT LCOV_PATH)
-    MESSAGE(WARNING "lcov not found!")
-ENDIF() # NOT LCOV_PATH
+if (UNIX)
 
-IF(NOT GENHTML_PATH)
-    MESSAGE(WARNING "genhtml not found!")
-ENDIF() # NOT GENHTML_PATH
+    IF(NOT LCOV_PATH)
+        MESSAGE(WARNING "lcov not found!")
+    ENDIF() # NOT LCOV_PATH
 
-add_custom_target(coverage
-        COMMAND "${CMAKE_BINARY_DIR}/bin/tests"
-        COMMAND ${LCOV_PATH} --directory ${CMAKE_BINARY_DIR} --capture --output-file ${CMAKE_BINARY_DIR}/coverage.info
-        COMMAND ${GENHTML_PATH} -o ${CMAKE_BINARY_DIR}/coverage/ ${CMAKE_BINARY_DIR}/coverage.info
-        )
+    IF(NOT GENHTML_PATH)
+        MESSAGE(WARNING "genhtml not found!")
+    ENDIF() # NOT GENHTML_PATH
+
+    add_custom_target(coverage
+            COMMAND "${CMAKE_BINARY_DIR}/bin/tests"
+            COMMAND ${LCOV_PATH} --directory ${CMAKE_BINARY_DIR} --capture --output-file ${CMAKE_BINARY_DIR}/coverage.info
+            COMMAND ${GENHTML_PATH} -o ${CMAKE_BINARY_DIR}/coverage/ ${CMAKE_BINARY_DIR}/coverage.info
+            )
+
+endif (UNIX)
