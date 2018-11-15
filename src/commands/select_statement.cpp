d4d6c6bba255fa461d203687fe70353b1e283c22
Boris Timofeenko <tmfn@feip.co>
2018-11-15 22:46:13 +1000

    Output formatting for SELECT

--- src/commands/select_statement.cpp
+++ src/commands/select_statement.cpp
@@ -1,5 +1,7 @@
 #include <select_statement.h>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 
 cmd::SelectStatement::SelectStatement() : SQLStatement(SELECT) {}
 
@@ -15,16 +17,27 @@ void cmd::SelectStatement::execute(st_e::IEngineStorage& storage_engine) {
     st_e::SelectAnswer t = type_ == ALL ? storage_engine.select_all(table_name_) :
            storage_engine.select(table_name_,cols_names_);
     std::string message;
-    for (const auto& col_name : t.columns_names){
-       message += col_name + " ";
-    }
-    message += '\n';
-    for (const auto row : t.rows){
-        for (const auto cell : row){
-            message += cell + " ";
+    std::stringstream message_stream;
+    std::vector <long long int> maxwidth (t.rows[0].size(),0);
+    for (auto &row : t.rows) {
+        for (auto cell = 0; cell < row.size(); ++cell) {
+            if (row[cell].size() >= maxwidth[cell]){
+                maxwidth[cell] = row[cell].size() + 1; //1 for spacing
+            }
         }
-        message += '\n';
     }
+    for (int i = 0; i < t.columns_names.size(); ++i){
+        message_stream << std::setw(maxwidth[i]) << t.columns_names[i];
+    }
+    message_stream << std::endl;
+    for (auto &row : t.rows) {
+        for (auto cell = 0; cell < row.size(); ++cell) {
+            message_stream << std::setw(maxwidth[cell]) << row[cell];
+        }
+        message_stream << std::endl;
+    }
+    message_stream << std::endl;
+    message = message_stream.str();
 //    for (int i = 0; t.rows.size() > i; ++i){
 //        for (int j = 0; t.rows[i].size() > j; ++j){
 //            message += t.rows[i][j] + " ";
