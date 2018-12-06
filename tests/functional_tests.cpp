48dcf61b42a30e002f4be50c2370b843e8e05e45
Boris Timofeenko <timbori@mail.ru>
2018-12-06 23:20:26 +1000

    Some functional tests

--- tests/functional_tests.cpp
+++ tests/functional_tests.cpp
@@ -18,27 +18,86 @@ int test_statement(const std::string& query, const std::string& output){
     std::string error_message;
     parser_driver.parse_string(query, error_message);
     parser_driver.sql_parser_result->execute();
-//    std::cout << "MESSAGES " << parser_driver.sql_parser_result->get_messages() << std::endl;
-//    std::cout << "RESULT " << parser_driver.result << std::endl;
+    std::cout << "MESSAGES " << parser_driver.sql_parser_result->get_messages() << std::endl;
+    std::cout << "RESULT " << parser_driver.result << std::endl;
     REQUIRE(compare(cleanoutput,parser_driver.sql_parser_result->get_messages()));
 }
 
 TEST_CASE("create insert select") {
-    test_statement("CREATE TABLE a(c1 INT, c2 INT);"
-                   "INSERT a(c1,c2) VALUES (12,14);"
-                   "SELECT c1,c2 FROM a;",
-                   "INSERT INTO a SUCCESSFUL.\n"
-                   "|c1|c2|\n"
-                   "|12|14|");
-    test_statement("CREATE TABLE a(c1 INT, c2 INT, c3 INT);"
-                   "INSERT a(c1,c2) VALUES (12,14);"
-                   "INSERT a(c1,c2,c3) VALUES (1,1746,177);"
-                   "SELECT c1,c2,c3 FROM a;",
-                   "INSERT INTO a SUCCESSFUL.\n"
-                   "INSERT INTO a SUCCESSFUL.\n"
-                   "|c1|c2|c3\n"
-                   "|12|  14|177"
-                   "| 1|1746|   ");
+
+    SECTION("create, insert, select *"){
+        test_statement("CREATE TABLE a(c1 INT, c2 INT);"
+                       "INSERT a(c1,c2) VALUES (12,14);"
+                       "SELECT * FROM a;",
+                       "|c1|c2|\n"
+                       "|12|14|");
+    }
+
+
+    SECTION("create, insert, select column_names") {
+        test_statement("CREATE TABLE a(c1 INT, c2 INT);"
+                       "INSERT a(c1,c2) VALUES (12,14);"
+                       "SELECT c1,c2 FROM a;",
+                       "|c1|c2|\n"
+                       "|12|14|");
+    }
+
+    SECTION("select formatting"){
+        test_statement("CREATE TABLE a(c1 INT, c2 INT, column3 INT);"
+                       "INSERT a(c1,c2,column3) VALUES (12,14,0);"
+                       "INSERT a(c1,c2,column3) VALUES (1,1746,177);"
+                       "SELECT c1,c2,column3 FROM a;",
+                       "INSERT INTO a SUCCESSFUL.\n"
+                       "INSERT INTO a SUCCESSFUL.\n"
+                       "|c1|  c2|column3|\n"
+                       "|12|  14|      0|\n"
+                       "| 1|1746|    177|");
+    }
+
+    SECTION("insert not all columns"){
+        test_statement("CREATE TABLE a(c1 INT, c2 INT, c3 INT);"
+                       "INSERT a(c1,c2) VALUES (12,14);"
+                       "INSERT a(c1,c2,c3) VALUES (1,1746,177);"
+                       "SELECT c1,c2,c3 FROM a;",
+                       "INSERT INTO a SUCCESSFUL.\n"
+                       "INSERT INTO a SUCCESSFUL.\n"
+                       "|c1|  c2| c3|\n"
+                       "|12|  14|177|\n"
+                       "| 1|1746|   |");
+    }
+
+    SECTION("insert into"){
+        test_statement("CREATE TABLE a(c1 INT, c2 INT);"
+                       "INSERT INTO a(c2,c1) VALUES (12,14);"
+                       "SELECT c1,c2 FROM a;",
+                       "INSERT INTO a SUCCESSFUL.\n"
+                       "|c1|c2|\n"
+                       "|12|14|");
+    }
+
+    SECTION("insert with columns inversion"){
+        test_statement("CREATE TABLE a(c1 INT, c2 INT, c3 INT);"
+                       "INSERT a(c2,c1) VALUES (12,14);"
+                       "INSERT a(c3,c1,c2) VALUES (12,14,13);"
+                       "SELECT * FROM a;",
+                       "INSERT INTO a SUCCESSFUL.\n"
+                       "|c1|c2|c3|\n"
+                       "|12|14|12|\n"
+                       "|14|13|  |");
+    }
+
+    SECTION("erase table before creation"){
+        //REQURE FAIL
+//        test_statement("CREATE TABLE a(c1 INT, c2 INT, column3 INT);"
+//                       "INSERT a(c1,c2,column3) VALUES (12,14,0);"
+//                       "INSERT a(c1,c2,column3) VALUES (1,1746,177);"
+//                       "SELECT c1,c2,c3 FROM a;",//this has to be undefined??
+//                       "INSERT INTO a SUCCESSFUL.\n"
+//                       "INSERT INTO a SUCCESSFUL.\n"
+//                       "|c1|  c2|column3|\n"
+//                       "|12|  14|      0|\n"
+//                       "| 1|1746|    177|");
+    }
 }
 
 TEST_CASE("insert pages") {
