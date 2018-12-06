47778f48c72386edc65350136dfd4fc3ba53df6b
Boris Timofeenko <timbori@mail.ru>
2018-12-06 23:45:47 +1000

    More functional tests

--- tests/functional_tests.cpp
+++ tests/functional_tests.cpp
@@ -18,9 +18,16 @@ int test_statement(const std::string& query, const std::string& output){
     std::string error_message;
     parser_driver.parse_string(query, error_message);
     parser_driver.sql_parser_result->execute();
-    std::cout << "MESSAGES " << parser_driver.sql_parser_result->get_messages() << std::endl;
-    std::cout << "RESULT " << parser_driver.result << std::endl;
+    //std::cout << "MESSAGES " << parser_driver.sql_parser_result->get_messages() << std::endl;
+    //std::cout << "RESULT " << parser_driver.result << std::endl;
     REQUIRE(compare(cleanoutput,parser_driver.sql_parser_result->get_messages()));
+}
+
+std::string repeat(int n, std::string s) {
+    std::ostringstream os;
+    for(int i = 0; i < n; i++)
+        os << s;
+    return os.str();
 }
 
 TEST_CASE("create insert select") {
@@ -87,28 +94,33 @@ TEST_CASE("create insert select") {
     }
 
     SECTION("erase table before creation"){
-        //REQURE FAIL
-//        test_statement("CREATE TABLE a(c1 INT, c2 INT, column3 INT);"
-//                       "INSERT a(c1,c2,column3) VALUES (12,14,0);"
-//                       "INSERT a(c1,c2,column3) VALUES (1,1746,177);"
-//                       "SELECT c1,c2,c3 FROM a;",//this has to be undefined??
-//                       "INSERT INTO a SUCCESSFUL.\n"
-//                       "INSERT INTO a SUCCESSFUL.\n"
-//                       "|c1|  c2|column3|\n"
-//                       "|12|  14|      0|\n"
-//                       "| 1|1746|    177|");
+        //REPLACE MESSAGE WITH REAL
+        test_statement("CREATE TABLE a(c1 INT, c2 INT, column3 INT);"
+                       "INSERT a(c1,c2,column3) VALUES (12,14,0);"
+                       "INSERT a(c1,c2,column3) VALUES (1,1746,177);"
+                       "SELECT c1,c2,c3 FROM a;",//c3 has to be undefined??
+                       "SOME ERROR MESSAGE");
     }
-}
 
-TEST_CASE("insert pages") {
-    roflan_parser::Driver parser_driver;
-    std::string error_message;
+    SECTION("big insert"){
+        roflan_parser::Driver parser_driver;
+        std::string error_message;
 
-    parser_driver.parse_string("create table test(id int, phone int);", error_message);
-    parser_driver.sql_parser_result->execute();
-    for (int i = 1; i < 20000; ++i) {
-        auto k = i-1;
-        parser_driver.parse_string("insert into test(id, phone) values (" + std::to_string(i) + ", " + std::to_string(k) + ");", error_message);
+        parser_driver.parse_string("create table test(id int, phone int);", error_message);
         parser_driver.sql_parser_result->execute();
+        for (int i = 1; i < 20000; ++i) {
+            auto k = i-1;
+            parser_driver.parse_string("insert into test(id, phone) values (" + std::to_string(i) + ", " + std::to_string(k) + ");", error_message);
+            parser_driver.sql_parser_result->execute();
+        }
     }
+
+    SECTION("big insert"){
+        std::string single_insert = "INSERT a(c1,c2,c3) VALUES (1,2,3);";
+        std::string single_select_output = "| 1| 2| 3|\n";
+        test_statement("CREATE TABLE a(c1 INT, c2 INT, c3 INT);" +
+                       repeat(1000,single_insert) + "SELECT * FROM a;",
+                       "|c1|c2|c3|\n"+repeat(1000,single_select_output));
+    }
+
 }
