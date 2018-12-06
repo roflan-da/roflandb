33f5d550b1f6ccdf6a1160133f23daaa49f5e4c7
Mup0c <mir-os@mail.ru>
2018-12-06 18:45:00 +1000

    rewrote query_conditions

--- src/query_conditions/inc/query_conditions.h
+++ src/query_conditions/inc/query_conditions.h
@@ -1,40 +1,85 @@
 #pragma once
 
+#include <string>
+#include <variant>
 #include <utility>
-#include "condition.h"
 #include <memory>
 
 namespace cond {
 
-    enum ComplexConditionType{
-        AND,
-        OR,
-        NOT,
-        SINGLE
-    };
+enum ConditionType{
+    AND,
+    OR,
+    NOT,
+    EQUAl,
+    NOT_EQUAL,
+    MORE,
+    LESS,
+    MORE_EQUAL,
+    LESS_EQUAL
+};
 
-    class QueryConditions {
-    public:
-        QueryConditions() = default;
-        QueryConditions(ComplexConditionType type,
-                std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> left,
-                std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> right) :
-                type_(type), left_(std::move(left)), right_(std::move(right)) {}
+//HOW TO USE DIS SHIT
+//auto condOR = cond::Condition();
+//auto cond = cond::Condition(cond::AND, "ababaca", 12);
+//std::cout << std::get<int>(cond.value());
 
-        void type(ComplexConditionType type) { type_ = type; }
-        ComplexConditionType type() const { return type_; }
 
-        void left(std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> left) { left_ = std::move(left); }
-        std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> left() { return left_; }
+class Condition {
+public:
+    Condition() = default;
+    Condition(ConditionType type) : type_(type) {}
+    virtual ~Condition() = default;
 
-        void right(std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> right) { right_ = std::move(right); }
-        std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> right() { return right_; }
+    void type(ConditionType type) { type_ = type; }
+    ConditionType type() const { return type_; }
 
-    private:
+private:
+    ConditionType type_;
 
-        ComplexConditionType type_ = SINGLE;
-        std::variant<std::shared_ptr<Condition>, std::shared_ptr<QueryConditions>> left_, right_;
 
-    };
+};
+
+
+//between column and value
+class SimpleCondition : Condition {
+public:
+    SimpleCondition() = default;
+    ~SimpleCondition() override = default ;
+    SimpleCondition(ConditionType type, std::string col_name, std::variant<int, std::string> value) :
+            Condition(type), column_name_(std::move(col_name)), value_(std::move(value)) {}
+
+    void column_name(std::string name) { column_name_ = name; }
+    std::string column_name() const { return column_name_; }
+
+    void value(std::variant<int, std::string> value) { value_ = value; }
+    std::variant<int, std::string> value() const { return value_; }
+
+
+private:
+    std::string column_name_;
+    std::variant<int, std::string> value_;
+
+};
+
+class ComplexCondition : Condition {
+public:
+    ComplexCondition() = default;
+    ~ComplexCondition() override = default;
+    ComplexCondition(ConditionType type, std::shared_ptr<Condition> left, std::shared_ptr<Condition> right) :
+            Condition(type), left_(left), right_(right) {}
+
+    void left(std::shared_ptr<Condition> left) { left_ = left; }
+    std::shared_ptr<Condition> left() { return left_; }
+
+    void right(std::shared_ptr<Condition> right) { right_ = right; }
+    std::shared_ptr<Condition> right() { return right_; }
+
+private:
+    std::shared_ptr<Condition> left_;
+    std::shared_ptr<Condition> right_;
+
+
+};
 
 }//namespace cond
