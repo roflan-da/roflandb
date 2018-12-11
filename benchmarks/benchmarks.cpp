ec7d964a70b697c2c39bdc70233e7907ef57f8e8
boriswinner <timbori@mail.ru>
2018-12-11 20:13:46 +1000

    added library for benchmarks

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -1,6 +1,14 @@
+#define BENCHPRESS_CONFIG_MAIN
+#include "benchpress.hpp"
 #include <iostream>
 #include "driver.h"
 
-int main(){
-    std::cout <<"benchmarks";
-}
+#include <chrono>
+#include <iostream>
+
+BENCHMARK("example", [](benchpress::context* ctx) {
+    for (size_t i = 0; i < ctx->num_iterations(); ++i) {
+        std::cout << "hello" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+})
