941273ca125a87607f1ff258052a10241cb9addf
Boris Timofeenko <timbori@mail.ru>
2019-01-18 20:56:24 +1000

    Draft for parallel testing

--- tests/functional_tests.cpp
+++ tests/functional_tests.cpp
@@ -5,6 +5,8 @@
 #include "inc/dtl/dtl.hpp"
 #include "row_check.h"
 #include "boost/filesystem.hpp"
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
 #include "iostream"
 
 static std::string TEST_DELIMETER = "---";
@@ -78,6 +80,43 @@ TEST_CASE("All tests"){
             REQUIRE(test_statement(query,result));
         }
     }
+}
+
+static std::string address = "127.0.0.1";
+static unsigned short port = 1337;
+int buffer_size = 1024;
+boost::asio::io_service service;
+boost::asio::ip::tcp::endpoint ep( boost::asio::ip::address::from_string(address), port);
+
+size_t read_complete(char * buf, const boost::system::error_code & err, size_t bytes) {
+    if ( err) return 0;
+    bool found = std::find(buf, buf + bytes, '\n') < buf + bytes;
+    // we read one-by-one until we get to enter, no buffering
+    return found ? 0 : 1;
+}
+void parallel_test_statement(const std::string &query, const std::string &output){
+    using namespace boost::asio;
+    std::string tquery = query+"\n";
+    ip::tcp::socket sock(service);
+    sock.connect(ep);
+    sock.write_some(buffer(tquery));
+    char buf[1024];
+    int bytes = read(sock, buffer(buf), boost::bind(read_complete,buf,_1,_2));
+    std::string copy(buf, bytes - 1);
+    tquery = tquery.substr(0, tquery.size() - 1);
+    std::cout << "server returned " << tquery << std::endl;
+    sock.close();
+}
+
+void test_parallel(){
+    using namespace boost::asio;
+    char* messages[] = { "CREATE TABLE a(c1 INT)", "CREATE TABLE b(c1 INT)", 0 };
+    boost::thread_group threads;
+    for ( char ** message = messages; *message; ++message) {
+        threads.create_thread( boost::bind(parallel_test_statement, *message));
+        boost::this_thread::sleep( boost::posix_time::millisec(100));
+    }
+    threads.join_all();
 }
 
 //TEST_CASE("insert not all columns","[.]"){
