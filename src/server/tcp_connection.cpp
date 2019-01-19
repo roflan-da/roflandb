e7ff3bf0055c2d4fa427ecc69f7dbc1d27d6c239
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-01-19 16:58:46 +1000

    remove commetns

--- src/server/tcp_connection.cpp
+++ src/server/tcp_connection.cpp
@@ -17,21 +17,15 @@ void TcpConnection::start() {
             boost::bind(
                     &TcpConnection::handle_read, shared_from_this(), placeholders::error, placeholders::bytes_transferred()));
 
-//    boost::asio::async_write(socket_, boost::asio::buffer(message_),
-//                             boost::bind(&TcpConnection::handle_write, shared_from_this(),
-//                                         boost::asio::placeholders::error,
-//                                         boost::asio::placeholders::bytes_transferred));
 }
 
 TcpConnection::TcpConnection(boost::asio::io_service& io_service) : socket_(io_service) {
 }
 
 void TcpConnection::handle_write(const boost::system::error_code&, size_t) {
-//    std::cout << "handle write" << std::endl << query_ << std::endl;
 }
 
 void TcpConnection::handle_read(const boost::system::error_code&, size_t) {
-//    std::cout << "handle read" << std::endl << query_ << std::endl;
     std::string error_message;
     streambuf::const_buffers_type bufs = query_.data();
     std::string query(boost::asio::buffers_begin(bufs),
